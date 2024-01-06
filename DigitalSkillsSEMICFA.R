67938659e045ed18fd16550afaa28a91bdfc5e60
Juan C. Correa <j.correa.n@gmail.com>
2024-01-06 14:56:00 -0600

    Update DigitalSkillsSEMICFA.R

--- DigitalSkillsSEMICFA.R
+++ DigitalSkillsSEMICFA.R
@@ -1,6 +1,6 @@
 library(readr)
 coil <- read_delim("coil.csv", delim = ";", escape_double = FALSE, trim_ws = TRUE)
-coil <- coil[51:54]
+coil <- data.frame(coil[51:54])
 library(lslx);
 modelData <- coil ;
 model<- "Digital_Skills :~> IN + PR + AP + EX
@@ -14,16 +14,8 @@ model<- "Digital_Skills :~> IN + PR + AP + EX
    AP~1;
    EX~1"
 
+lslxCFA <- lslx$new(model = model, data = modelData)
 
-IN <- c(0.985, 0.72, 0.616, 0.477)
-PR <- c(0.72, 0.985, 0.748, 0.58)
-AP <- c(0.616, 0.748, 0.985, 0.496)
-EX <- c(0.477, 0.588, 0.496, 0.985)
-samplecov <- rbind(IN, PR, AP, EX)
-colnames(samplecov) <- c("IN", "PR", "AP", "EX")
-
-lslxCFA <- lslx$new(model = model, sample_cov = samplecov, sample_size = 65)
-vcov(lslxCFA, selector = "aic")
 moment_jacobian <- lslxCFA$extract_moment_jacobian(selector = "bic", type = "effective")
 
 # Following the suggestions of Po-Hsien Huang (2020),
@@ -34,5 +26,3 @@ moment_jacobian <- lslxCFA$extract_moment_jacobian(selector = "bic", type = "eff
 lslxCFA$fit(penalty_method = "mcp", lambda_grid = seq(0.81, 0.62), delta_grid = seq(0.110,0.600))
 
 
-result<-lavaan(model, data=modelData, fixed.x=FALSE, missing="FIML");
-summary(result, fit.measures=TRUE);
