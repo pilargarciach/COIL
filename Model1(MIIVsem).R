0341de8171e87e6536382869c4bf09f877946904
Juan C. Correa <j.correa.n@gmail.com>
2024-01-11 02:44:50 -0600

    Update Model1(MIIVsem).R

--- Model1(MIIVsem).R
+++ Model1(MIIVsem).R
@@ -54,10 +54,61 @@ model1 <- '
    CD3~1;
    CD4~1;
    CD5~1;'
+
+model1_fit <- sem(model = model1, data = coildata, meanstructure = TRUE)
+model1_params <- parameterEstimates(model1_fit) 
+write.csv(model1_params, "model1_params.csv")
+
 library(MIIVsem)
 miivs(model1)
-miive(model = model1, data = data)
+model1_miiv <- miive(model = model1, data = coildata)
+Model1_miiv <- estimatesTable(model1_miiv)
+write.csv(Model1_miiv, "Model1_miiv.csv")
+
+ML.lhs <- model1_params$lhs
+ML.rhs <- model1_params$rhs
+ML.es <- model1_params$est 
+ML.se <- model1_params$se
+ML.p <- model1_params$pvalue
+lava1 <- data.frame(ML.lhs, ML.rhs, ML.es, ML.se, ML.p)
+colnames(lava1)[1] <- "lhs"
+colnames(lava1)[2] <- "rhs"
+
+MIIVE.lhs <- Model1_miiv$lhs
+MIIVE.rhs <- Model1_miiv$rhs
+MIIVE.es <- Model1_miiv$est
+MIIVE.se <- Model1_miiv$se
+MIIVE.p <- Model1_miiv$pvalue
+miiv1 <- data.frame(MIIVE.lhs, MIIVE.rhs, MIIVE.es, MIIVE.se, MIIVE.p)
+colnames(miiv1)[1] <- "lhs"
+colnames(miiv1)[2] <- "rhs"
+
+## merge lava1 and miiv1
+MethodsComparison <- merge(lava1, miiv1, by = c("lhs", "rhs"), all.x = TRUE, all.y = TRUE)
 
 
-model1_miiv <- miive(model = model1, data = data)$dat
-write.csv(model1_miiv, "~/Desktop/model1_miiv.csv")
+## extract lavaan fit estimates
+chi              <- round(fitMeasures(model1_fit)["chisq"], 2)
+chidf            <- fitMeasures(model1_fit)["df"]
+chip             <- round(fitMeasures(model1_fit)["pvalue"], 2)
+
+## extract MIIVsem fit estimates
+sarg             <- round(model1_miiv[12, "Sargan"],2)
+sargdf           <- model1_miiv[19, "df"]
+sargp            <- round(model1_miiv[19, "P(Chi)"], 2)
+
+## create comparison table for display
+likchi_lavaan    <- paste0(chi, ", df = ", chidf, ", p = ", chip)
+eq1sarg_lavaan   <- "---------------------"
+eq2sarg_lavaan   <- "---------------------"
+likchi_miiv      <- "---------------------"
+eq1sarg_miiv     <- paste0("0 (exactly identified)")
+eq2sarg_miiv     <- paste0(sarg, ", df = ", sargdf, ", p = ", sargp)
+col1             <- rbind(likchi_lavaan, eq1sarg_lavaan, eq2sarg_lavaan)
+col2             <- rbind(likchi_miiv, eq1sarg_miiv, eq2sarg_miiv)
+fit0             <- cbind(col1, col2)
+rownames(fit0)  <- c("Model 1", "Model 1: educ equation", "Model 1: prestg80 equation")
+colnames(fit0)  <- c("Likelihood Chi Square", "Sargan Chi Square")
+
+print(mod1)
+print(fit0, quote = FALSE, right = FALSE, row.names = TRUE)
